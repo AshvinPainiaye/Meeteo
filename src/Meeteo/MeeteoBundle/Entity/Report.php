bf95d2d5079f135ee54d3ff22e02f403654adb87
Iudo <Iudo@users.noreply.github.com>
2016-07-06 15:43:28 +0400

    Merge origin/master
    Conflicts:
    	app/Resources/views/base.html.twig

--- src/Meeteo/MeeteoBundle/Entity/Report.php
+++ src/Meeteo/MeeteoBundle/Entity/Report.php
@@ -68,7 +68,16 @@ class Report
      *
      * @ORM\Column(name="photo", type="string", length=255, nullable=true)
      */
-    private $photo;
+    private $imageName;
+    
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="report_image", fileNameProperty="imageName")
+     * 
+     * @var File
+     */
+    private $imageFile;
     
     /**
      * @ORM\ManyToOne(targetEntity="User", inversedBy="id")
@@ -236,9 +245,9 @@ class Report
      *
      * @return Report
      */
-    public function setPhoto($photo)
+    public function setImageName($photo)
     {
-        $this->photo = $photo;
+        $this->imageName = $photo;
 
         return $this;
     }
@@ -248,9 +257,41 @@ class Report
      *
      * @return string
      */
-    public function getPhoto()
+    public function getImageName()
     {
-        return $this->photo;
+        return $this->imageName;
+    }
+    
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return Product
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTime('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 }
 
