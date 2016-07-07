494d33e6acaf92def7f163cba1dadf8c149c5f43
Iudo <Iudo@users.noreply.github.com>
2016-07-07 12:23:43 +0400

    Merge origin/master
    Conflicts:
    	app/Resources/views/base.html.twig

---
+++ src/Meeteo/MeeteoBundle/EventListener/RegistrationListener.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Meeteo\MeeteoBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class RegistrationListener implements EventSubscriberInterface {
+
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router) {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents() {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
+        );
+    }
+
+    public function onRegistrationSuccess(FormEvent $event) {
+
+        /** @var $user \FOS\UserBundle\Model\UserInterface */
+        $user = $event->getForm()->getData();
+
+        $user->addRole('ROLE_METEOROLOGUE');
+
+        $url = $this->router->generate('homepage');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+
+}
