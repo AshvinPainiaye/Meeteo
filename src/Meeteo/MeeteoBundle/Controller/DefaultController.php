56be7fedbce441d39ca462cc3b53061005f127b5
AshFromReunion <ash-concept@hotmail.fr>
2016-07-11 11:33:04 +0400

    multiple marker

--- src/Meeteo/MeeteoBundle/Controller/DefaultController.php
+++ src/Meeteo/MeeteoBundle/Controller/DefaultController.php
@@ -1,25 +1,17 @@
 <?php
-
 namespace Meeteo\MeeteoBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
-
     public function indexAction() {
-
         $repository = $this->getDoctrine()
                 ->getRepository('MeeteoMeeteoBundle:Report');
-
         $listereports = $repository->findAll();
-
-        if (null !== $listereports) {
+        if (!$listereports) {
             throw $this->createNotFoundException("Aucun rapport à afficher");
         }
-
         return $this->render('MeeteoMeeteoBundle:Default:index.html.twig', array(
                     'listereports' => $listereports
         ));
     }
-
 }
