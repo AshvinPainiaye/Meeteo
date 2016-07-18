0f444e88a2485f5e4688f4ebd9a14012bcfe0767
JFrederic <frederic-jouan97440@outlook.fr>
2016-07-18 15:13:01 +0400

    maj

--- src/Meeteo/MeeteoBundle/Controller/DefaultController.php
+++ src/Meeteo/MeeteoBundle/Controller/DefaultController.php
@@ -16,5 +16,46 @@ class DefaultController extends Controller {
         return $this->render('MeeteoMeeteoBundle:Default:index.html.twig', array(
                     'listereports' => $listereports
         ));
+
+
     }
+
+
+    public function VentsAction() {
+
+      // Create em for entity Report
+      $reportRepository = $this->getDoctrine()
+              ->getRepository('MeeteoMeeteoBundle:Report');
+      // Get all report from database
+      $listereports = $reportRepository->findAll();
+      // If null raise execption
+      // if (!$listereports) {
+      //     throw $this->createNotFoundException("Aucun rapport à afficher");
+      // }
+      // Return listereports to view index.html.twig
+
+      return $this->render('MeeteoMeeteoBundle:Maps:Vents.html.twig', array(
+                  'listereports' => $listereports
+      ));
+    }
+
+    public function TemperatureAction() {
+
+      // Create em for entity Report
+      $reportRepository = $this->getDoctrine()
+              ->getRepository('MeeteoMeeteoBundle:Report');
+      // Get all report from database
+      $listereports = $reportRepository->findAll();
+      // If null raise execption
+      // if (!$listereports) {
+      //     throw $this->createNotFoundException("Aucun rapport à afficher");
+      // }
+      // Return listereports to view index.html.twig
+
+      return $this->render('MeeteoMeeteoBundle:Maps:temperature.html.twig', array(
+                  'listereports' => $listereports
+      ));
+    }
+
+
 }
