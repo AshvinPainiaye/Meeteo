3e7de5bb8f6a750c0483ced76f6d548b03a9e93a
AshFromReunion <ash-concept@hotmail.fr>
2016-07-18 14:58:20 +0400

    maj

--- src/Meeteo/MeeteoBundle/Controller/ManageController.php
+++ src/Meeteo/MeeteoBundle/Controller/ManageController.php
@@ -11,19 +11,23 @@ class ManageController extends Controller
         // Create em for entity user
         $usersRepository = $this->getDoctrine()
                 ->getRepository('MeeteoMeeteoBundle:User');
-        
+
         // Get all users from database
         $listeUsers = $usersRepository->findAll();
-        
+
         // If null no user raise execption
-        if (!$listeUsers) {
-            throw $this->createNotFoundException("Aucun Utilisateur à afficher");
-        }
-        
+        // if (!$listeUsers) {
+        //     throw $this->createNotFoundException("Aucun Utilisateur à afficher");
+        // }
+
         // Return listeusers to view manage.html.twig
         return $this->render('MeeteoMeeteoBundle:Manage:manage.html.twig', array(
                     'listeUsers' => $listeUsers
         ));
+    }
+
+    public function EditAction(){
+
     }
 
 }
--- src/Meeteo/MeeteoBundle/Controller/ReportController.php
+++ src/Meeteo/MeeteoBundle/Controller/ReportController.php
@@ -10,61 +10,112 @@ use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ReportController extends Controller {
 
-    public function ReportAction(Request $request, $lat, $lon) {
-        // Create a report and give it a date, latitude, longitude
-        $report = new Report();
-        $report->setDate(new \DateTime('now'))
-                ->setLat($lat)
-                ->setLon($lon);
-        // TODO: Create a class for this form
-        $form = $this->createFormBuilder($report)
-                ->add('weather', ChoiceType::class, array(
-                    'choices' => array(
-                        'Ensoleillé' => 'Ensoleillé',
-                        'Nuageux' => 'Nuageux',
-                        'Pluvieux' => 'Pluvieux',
-                        'Brume' => 'Brume',
-                        'Neige' => 'Neige',
-                        'Grèle' => 'Grèle',
-                    ),
-                ))
-                ->add('temperature', ChoiceType::class, array(
-                    'choices' => array(
-                        'Très froid' => 'Très froid',
-                        'Froid' => 'Froid',
-                        'Idéale' => 'Idéale',
-                        'Chaud' => 'Chaud',
-                        'Très chaud' => 'Très chaud',
-                    ),
-                ))
-                ->add('wind', ChoiceType::class, array(
-                    'choices' => array(
-                        'Pas de vent' => 'Pas de vent',
-                        'Vent modéré' => 'Vent modéré',
-                        'Vent assez fort' => 'Vent assez fort',
-                        'Vent fort' => 'Vent fort',
-                        'Cyclone' => 'Cyclone',
-                    ),
-                ))
-                ->add('imageFile', VichImageType::class, array('label' => 'Photo','required' => false))
-                ->getForm();
+  public function ReportAction(Request $request, $lat, $lon) {
+    // Create a report and give it a date, latitude, longitude
+    $report = new Report();
+    $report->setDate(new \DateTime('now'))
+    ->setLat($lat)
+    ->setLon($lon);
+    // TODO: Create a class for this form
+    $form = $this->createFormBuilder($report)
+    ->add('weather', ChoiceType::class, array(
+      'choices' => array(
+        'Ensoleillé' => 'Ensoleillé',
+        'Nuageux' => 'Nuageux',
+        'Pluvieux' => 'Pluvieux',
+        'Brume' => 'Brume',
+        'Neige' => 'Neige',
+        'Grèle' => 'Grèle',
+      ),
+    ))
+    ->add('temperature', ChoiceType::class, array(
+      'choices' => array(
+        '0°' => '0°',
+        '1°' => '1°',
+        '2°' => '2°',
+        '3°' => '3°',
+        '4°' => '4°',
+        '5°' => '5°',
+        '6°' => '6°',
+        '7°' => '7°',
+        '8°' => '8°',
+        '9°' => '9°',
+        '10°' => '10°',
+        '11°' => '11°',
+        '12°' => '12°',
+        '13°' => '13°',
+        '14°' => '14°',
+        '15°' => '15°',
+        '16°' => '16°',
+        '17°' => '17°',
+        '18°' => '18°',
+        '19°' => '19°',
+        '20°' => '20°',
+        '21°' => '21°',
+        '22°' => '22°',
+        '23°' => '23°',
+        '24°' => '24°',
+        '25°' => '25°',
+        '26°' => '26°',
+        '27°' => '27°',
+        '28°' => '28°',
+        '29°' => '29°',
+        '30°' => '30°',
+        '31°' => '31°',
+        '32°' => '32°',
+        '33°' => '33°',
+        '34°' => '34°',
+        '35°' => '35°',
+        '36°' => '36°',
+        '37°' => '37°',
+        '38°' => '38°',
+        '39°' => '39°',
+        '40°' => '40°',
+        '41°' => '41°',
+        '42°' => '42°',
+        '43°' => '43°',
+        '44°' => '44°',
+        '45°' => '45°',
+        '46°' => '46°',
+        '47°' => '47°',
+        '48°' => '48°',
+        '49°' => '49°',
+        '50°' => '50°',
 
-        $form->handleRequest($request);
+        // 'Froid' => 'Froid',
+        // 'Idéale' => 'Idéale',
+        // 'Chaud' => 'Chaud',
+        // 'Très chaud' => 'Très chaud',
+      ),
+    ))
+    ->add('wind', ChoiceType::class, array(
+      'choices' => array(
+        'Pas de vent' => 'Pas de vent',
+        'Vent modéré' => 'Vent modéré',
+        'Vent assez fort' => 'Vent assez fort',
+        'Vent fort' => 'Vent fort',
+        'Cyclone' => 'Cyclone',
+      ),
+    ))
+    ->add('imageFile', VichImageType::class, array('label' => 'Photo','required' => false))
+    ->getForm();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Get connected user
-            $user = $this->get('security.token_storage')->getToken()->getUser();
-            // Set user for report, save to database and redirect to homepage
-            $report->setUser($user);
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($report);
-            $em->flush();
-            return $this->redirectToRoute('meeteo_meeteo_homepage');
-        }
-        // Create the form view
-        return $this->render('MeeteoMeeteoBundle:Report:report.html.twig', array(
-                    'form' => $form->createView(),
-        ));
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      // Get connected user
+      $user = $this->get('security.token_storage')->getToken()->getUser();
+      // Set user for report, save to database and redirect to homepage
+      $report->setUser($user);
+      $em = $this->getDoctrine()->getEntityManager();
+      $em->persist($report);
+      $em->flush();
+      return $this->redirectToRoute('meeteo_meeteo_homepage');
     }
+    // Create the form view
+    return $this->render('MeeteoMeeteoBundle:Report:report.html.twig', array(
+      'form' => $form->createView(),
+    ));
+  }
 
 }
