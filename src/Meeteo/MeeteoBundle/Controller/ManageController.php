0a62b7c61d613912a6b9ba8e08da3c7bb3f3ae98
AshFromReunion <ash-concept@hotmail.fr>
2016-07-20 11:18:34 +0400

    refonte graphique

--- src/Meeteo/MeeteoBundle/Controller/ManageController.php
+++ src/Meeteo/MeeteoBundle/Controller/ManageController.php
@@ -2,10 +2,7 @@
 
 namespace Meeteo\MeeteoBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Meeteo\MeeteoBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 
 class ManageController extends Controller
 {
@@ -29,27 +26,8 @@ class ManageController extends Controller
         ));
     }
 
+    public function EditAction(){
 
-    public function EditAction($id,Request $request){
-      $em = $this->getDoctrine()->getManager();
-      $user= $em->getRepository('MeeteoMeeteoBundle:User')->find($id);
-
-      $editForm = $this->createFormBuilder($user)
-      ->add('username', 'text')
-      ->add('email', 'text')
-      ->add('save', 'submit')
-      ->getForm();
-
-      $editForm->handleRequest($request);
-
-      if ($editForm->isSubmitted() && $editForm->isValid()) {
-        $em->flush();
-
-        return $this->redirectToRoute('_manage');
-        }
-        $build['edit_form'] = $editForm->createView();
-
-        return $this->render('MeeteoMeeteoBundle:Edit:edit.html.twig',$build );
     }
 
 }
--- src/Meeteo/MeeteoBundle/Controller/ReportController.php
+++ src/Meeteo/MeeteoBundle/Controller/ReportController.php
@@ -27,7 +27,6 @@ class ReportController extends Controller {
         'Neige' => 'Neige',
         'Grèle' => 'Grèle',
       ),
-
     ))
     ->add('temperature', ChoiceType::class, array(
       'choices' => array(
@@ -82,6 +81,11 @@ class ReportController extends Controller {
         48 => 48,
         49 => 49,
         50 => 50,
+
+        // 'Froid' =>'Froid',
+        // 'Idéale' => 'Idéale',
+        // 'Chaud' => 'Chaud',
+        // 'Très chaud' => 'Très chaud',
       ),
     ))
     ->add('wind', ChoiceType::class, array(
