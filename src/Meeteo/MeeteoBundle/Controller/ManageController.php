769b125f67c34e021ae5dfc5ce00ae70d5f4b48e
Iudo <Iudo@users.noreply.github.com>
2016-07-21 11:02:59 +0400

    security.authorization_checker instead of security.context

--- src/Meeteo/MeeteoBundle/Controller/ManageController.php
+++ src/Meeteo/MeeteoBundle/Controller/ManageController.php
@@ -1,48 +1,66 @@
 <?php
+
 namespace Meeteo\MeeteoBundle\Controller;
+
 use Symfony\Component\HttpFoundation\Request;
-use Meeteo\MeeteoBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-class ManageController extends Controller
-{
-    public function ManageAction()
-    {
-        // Create em for entity user
-        $usersRepository = $this->getDoctrine()
-                ->getRepository('MeeteoMeeteoBundle:User');
-        // Get all users from database
-        $listeUsers = $usersRepository->findAll();
-        // If null no user raise execption
-        // if (!$listeUsers) {
-        //     throw $this->createNotFoundException("Aucun Utilisateur à afficher");
-        // }
+
+class ManageController extends Controller {
+
+    public function ManageAction() {
+        // Get our "authorization_checker" Object
+        $auth_checker = $this->get('security.authorization_checker');
+        // Get our Token (representing the currently logged in user)
+        //$token = $this->get('security.token_storage')->getToken();
+        // Get our user from that token
+        //$user = $token->getUser();
+        // Check for Roles on the $auth_checker
+        $isRoleAdmin = $auth_checker->isGranted('ROLE_ADMIN');
+
+        if ($isRoleAdmin) {
+            // Create em for entity user
+            $usersRepository = $this->getDoctrine()
+                    ->getRepository('MeeteoMeeteoBundle:User');
+            // Get all users from database
+            $listeUsers = $usersRepository->findAll();
+        }
+
         // Return listeusers to view manage.html.twig
         return $this->render('MeeteoMeeteoBundle:Manage:manage.html.twig', array(
                     'listeUsers' => $listeUsers
         ));
     }
-    public function EditAction($id,Request $request){
-      $em = $this->getDoctrine()->getManager();
-      $user= $em->getRepository('MeeteoMeeteoBundle:User')->find($id);
-      $editForm = $this->createFormBuilder($user)
-      ->add('username', 'text')
-      ->add('email', 'text')
-      ->add('roles', 'choice', array(
-                  'multiple' => true,
-                  'expanded' => true,
-                  'choices' => array(
-                    'ROLE_ADMIN'=>'ADMIN',
-                      'ROLE_METEOROLOGUE'=>'METEOROLOGUE',
-                  ),
-              ))
-      // ->add('save' , 'submit')
-      ->getForm();
-      $editForm->handleRequest($request);
-      if ($editForm->isSubmitted() && $editForm->isValid()) {
-        $em->flush();
-        return $this->redirectToRoute('_manage');
+
+    public function EditAction($id, Request $request) {
+
+        // Get our "authorization_checker" Object
+        $auth_checker = $this->get('security.authorization_checker');
+        // Check for Roles on the $auth_checker
+        $isRoleAdmin = $auth_checker->isGranted('ROLE_ADMIN');
+        // Test if user have ROLE_ADMIN
+        if ($isRoleAdmin) {
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository('MeeteoMeeteoBundle:User')->find($id);
+            $editForm = $this->createFormBuilder($user)
+                    ->add('username', 'text')
+                    ->add('email', 'text')
+                    ->add('roles', 'choice', array(
+                        'multiple' => true,
+                        'expanded' => true,
+                        'choices' => array(
+                            'ROLE_ADMIN' => 'ADMIN',
+                            'ROLE_METEOROLOGUE' => 'METEOROLOGUE',
+                        ),
+                    ))
+                    ->getForm();
+            $editForm->handleRequest($request);
+            if ($editForm->isSubmitted() && $editForm->isValid()) {
+                $em->flush();
+                return $this->redirectToRoute('_manage');
+            }
+            $build['edit_form'] = $editForm->createView();
+            return $this->render('MeeteoMeeteoBundle:Edit:edit.html.twig', $build);
         }
-        $build['edit_form'] = $editForm->createView();
-        return $this->render('MeeteoMeeteoBundle:Edit:edit.html.twig',$build );
     }
+
 }
